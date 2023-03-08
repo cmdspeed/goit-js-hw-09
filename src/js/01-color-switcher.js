3c6b2cb34dc96ba31e628f6fbcd23b11777641e8
cmdspeed <cmdspeed@gmail.com>
2023-03-08 18:05:21 +0100

    task 01 end

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let timer;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+stopBtn.setAttribute('disabled', '');
 
+function changeBgColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+startBtn.addEventListener('click', () => {
+    startBtn.setAttribute('disabled', '');
+    stopBtn.removeAttribute('disabled');
+    timer = setInterval(changeBgColor, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', '');
+    clearInterval(timer);
+});
