6f68ee090936cbd9bd174b294c47237984746ce7
cmdspeed <cmdspeed@gmail.com>
2023-03-11 17:20:24 +0100

    task 03 end

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  let delay = document.querySelector('input[name="delay"]').valueAsNumber;
+  const step = document.querySelector('input[name="step"]').valueAsNumber;
+  const amount = document.querySelector('input[name="amount"]').valueAsNumber;
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
   }
-}
+
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
+
+  event.currentTarget.reset();
+});
